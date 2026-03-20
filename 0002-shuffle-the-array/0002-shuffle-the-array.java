1e3d49b72b1b715332633f01a10ab9b119884ba7
Sri Poojitha <121282475+poojitha-devhub@users.noreply.github.com>
2026-03-20 20:33:41 +0530

    03-20-2026 0002-shuffle-the-array Easy .java 0 ms (100%) 46.7 MB (18.26%)

---
+++ 0002-shuffle-the-array/0002-shuffle-the-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int i=0;
+        int j=n;
+        int ans[]=new int[2*n];
+        for(int k=0;k<2*n;k=k+2){
+            ans[k]=nums[i];
+            ans[k+1]=nums[j];
+            i++;
+            j++;
+        }
+        return ans;
+
+        
+    }
+}
