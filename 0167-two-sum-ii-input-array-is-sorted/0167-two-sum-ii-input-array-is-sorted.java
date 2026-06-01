728d7cc989778467de60533726b28f1c8c1c5040
Sri Poojitha <121282475+poojitha-devhub@users.noreply.github.com>
2026-06-01 17:05:46 +0530

    06-01-2026 0167-two-sum-ii-input-array-is-sorted Medium .java 2 ms (96.36%) 48.5 MB (41.06%)

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int[] ans=new int[2];
+        int[] answer=new int[2];
         int i=0;
         int j=numbers.length-1;
         while(i<j){
-            int sum= numbers[i]+numbers[j];
+            int sum=numbers[i]+numbers[j];
             if(sum<target){
                 i++;
             }
@@ -12,11 +12,11 @@ class Solution {
                 j--;
             }
             else{
-                ans[0]=i+1;
-                ans[1]=j+1;
-                return ans;
+                answer[0]=i+1;
+                answer[1]=j+1;
+                return answer;
             }
         }
-        return ans;  
+        return answer;
     }
 }
