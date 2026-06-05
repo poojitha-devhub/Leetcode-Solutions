217344a9441143abafc67d15c3738300e48590b3
Sri Poojitha <121282475+poojitha-devhub@users.noreply.github.com>
2026-06-05 12:19:07 +0530

    06-05-2026 0053-maximum-subarray Medium .java 1 ms (99.95%) 77.4 MB (31.2%)

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        int curr=0;
+        for(int i=0;i<nums.length;i++){
+            curr=curr+nums[i];
+            max=Math.max(curr,max);
+            if(curr<0){
+                curr=0;
+            }  
+        }
+        return max;
+    }
+}
