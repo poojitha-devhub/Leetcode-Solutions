abc8d1820a797290f2325760208bbf67035c8fea
Sri Poojitha <121282475+poojitha-devhub@users.noreply.github.com>
2026-06-06 18:56:51 +0530

    06-06-2026 2965-find-missing-and-repeated-values Easy .java 1 ms (100%) 47.4 MB (39.71%)

---
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int ans[]=new int[2];
+        int n=grid.length;
+        int size=n*n;
+        int freq[]=new int[size+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                freq[grid[i][j]]++;
+            }
+        }
+        for(int i=1;i<freq.length;i++){
+            if(freq[i]==2){
+                ans[0]=i;
+            }
+            if(freq[i]==0){
+                ans[1]=i;
+            }
+        }
+        return ans;
+        
+    }
+}
