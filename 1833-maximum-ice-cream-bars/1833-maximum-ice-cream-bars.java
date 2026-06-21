77905cb8ecf5ec2a12b29cc2d4ae4012cf563dd7
Sri Poojitha <121282475+poojitha-devhub@users.noreply.github.com>
2026-06-21 11:58:23 +0530

    06-21-2026 1833-maximum-ice-cream-bars Medium .java 38 ms (24.21%) 84 MB (43.59%)

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        int ans=0;
+        Arrays.sort(costs);
+        for(int i=0;i<costs.length;i++){
+            if(costs[i]<=coins){
+                coins-=costs[i];
+                ans++;
+            } 
+        }
+        
+        return ans;
+        
+    }
+}
