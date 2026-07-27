ba8b17e091747d7b2beeb4537095369fa4523723
Sri Poojitha <121282475+poojitha-devhub@users.noreply.github.com>
2026-07-27 14:49:03 +0530

    07-27-2026 0234-palindrome-linked-list Easy .java 6 ms (35.46%) 94.5 MB (44.08%)

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,40 +10,41 @@
  */
 class Solution {
     public ListNode findmiddle(ListNode head){
-        ListNode hare=head;
-        ListNode turtle=head;
-        while(hare.next!=null && hare.next.next!=null){
-            hare=hare.next.next;
-            turtle=turtle.next;
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        return turtle;
+        return slow;
     }
     public ListNode reverse(ListNode head){
         ListNode prev=null;
         ListNode curr=head;
+        ListNode next;
         while(curr!=null){
-            ListNode next=curr.next;
+            next=curr.next;
             curr.next=prev;
             prev=curr;
             curr=next;
         }
-        return prev;
+        head=prev;
+        return head;
     }
     public boolean isPalindrome(ListNode head) {
         if(head==null || head.next==null){
             return true;
         }
         ListNode middle=findmiddle(head);
-        ListNode secondstart=reverse(middle.next);
-        ListNode firststart=head;
-        while(secondstart!=null){
-            if(firststart.val!=secondstart.val){
+        ListNode secondlist=reverse(middle.next);
+        ListNode firstlist=head;
+        while(secondlist!=null){
+            if(firstlist.val!=secondlist.val){
                 return false;
             }
-            firststart=firststart.next;
-            secondstart=secondstart.next;
+            secondlist=secondlist.next;
+            firstlist=firstlist.next;
         }
         return true;
-        
     }
 }
