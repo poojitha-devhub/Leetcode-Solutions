d2f52feae1cda703f122125eeb3638adf74043ec
Sri Poojitha <121282475+poojitha-devhub@users.noreply.github.com>
2026-07-27 14:23:14 +0530

    07-27-2026 0206-reverse-linked-list Easy .java 0 ms (100%) 44.4 MB (14.07%)

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,20 +10,19 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head==null ||head.next==null){
+        if(head==null || head.next==null){
             return head;
         }
-        ListNode prevnode=head;
-        ListNode currnode=head.next;
-        while(currnode!=null){
-            ListNode nextnode=currnode.next;
-            currnode.next=prevnode;
-            prevnode=currnode;
-            currnode=nextnode;
+        ListNode prev=null;
+        ListNode curr=head;
+        ListNode next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
         }
-        head.next=null;
-        head=prevnode;
+        head=prev;
         return head;
-        
     }
 }
